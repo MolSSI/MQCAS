82d70fd24870fc45bd67da392711bfd1389b10a1
Matthew Welborn <mattgwelborn@gmail.com>
2020-03-13 10:47:51 -0400

    Edit tagline and add description

--- modifications/2020-03-12-ASCDB-tagline/add_tagline.py
+++ modifications/2020-03-12-ASCDB-tagline/add_tagline.py
@@ -3,7 +3,10 @@ import qcportal as ptl
 client = ptl.FractalClient.from_file()
 
 ds = client.get_collection("ReactionDataset", "ASCDB")
-ds.data.__dict__["tagline"] = (
+ds.data.__dict__[
+    "tagline"
+] = "A statistically significant database generated from MGCDB84, GMTKN55, and Minnesota 2015B."
+ds.data.__dict__["description"] = (
     "A small database of statistically significant chemical properties ranging from "
     "transition metals to artificial molecules coming from the three largest computational databases in the literature: "
     "MGCDB84, GMTKN55, and Minnesota 2015B."
